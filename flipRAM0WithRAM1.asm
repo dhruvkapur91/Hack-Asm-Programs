e44c7c8bd62b3a1b85733f247b2fa55b054f242e
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-08-21 17:09:22 +0530

    Swapping logic. Use variable for first time.

---
+++ flipRAM0WithRAM1.asm
@@ -0,0 +1,31 @@
+// Flip RAM0 with RAM1
+
+// temp = RAM0
+// RAM0 = RAM1
+// RAM1 = temp
+
+// Set temp to RAM0
+	@R0
+	D=M
+	@temp  // So temp has RAM0
+	M=D
+
+// Put RAM1 in D
+	@R1
+	D=M
+// Put D in RAM0
+	@R0
+	M=D
+
+// Put temp in D, which is RAM0
+	@temp
+	D=M
+
+// Put D in RAM1
+	@R1
+	M=D
+
+(END)
+	@END
+	0;JMP
+	
